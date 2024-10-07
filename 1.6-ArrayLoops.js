ad56dd7dd77eb25c466ec4809b3a9e6fb887500e
Mesadova <emesadova@hotmail.com>
2024-10-07 10:33:23 +0200

    Nivell 2

--- 1.6-ArrayLoops.js
+++ 1.6-ArrayLoops.js
@@ -21,3 +21,27 @@ const newArray = array.filter((number) => number % 2 == 0);
 console.log('Exercici 3:');
 console.log(newArray);
 
+
+// === Nivell 2 ===
+// === Exercici 4 ===
+
+let obj = { nom: "Ona", edat: 25, ciutat: 'Barcelona' };
+console.log('Exercici 4:');
+
+for (const property in obj) {
+    console.log(`${property}: ${obj[property]}`);
+};
+
+
+// === Exercici 5 ===
+
+let numeros = [1, 2, 3, 4, 5, 6];
+console.log('Exercici 5:');
+
+for (const num of numeros) {
+    if (num == 5) {
+        break;
+    }
+    console.log(num);
+};
+

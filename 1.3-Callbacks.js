4c99c071a5f8430128457916662c63c228a9f803
Mesadova <emesadova@hotmail.com>
2024-10-07 10:15:47 +0200

    Nivell 2

--- 1.3-Callbacks.js
+++ 1.3-Callbacks.js
@@ -23,3 +23,33 @@ suma = (a, b) => a + b;
 
 console.log('Exercici 2:');
 console.log(calculadora(2, 4, suma));
+
+
+// === Nivell 2 ===
+// === Exercici 3 ===
+
+esperarISaludar = (nom, funct) => {
+  setTimeout(() => {
+    funct(nom);
+  }, "2000");
+}
+
+nomPropi = 'Eduard'
+
+saludar = (nom) => console.log(`Exercici 3: Hola, ${nom}`);
+esperarISaludar(nomPropi, saludar);
+
+
+// === Exercici 4 ===
+
+processarElements = (array, funct) => {
+  array.forEach(element => {
+    funct(element);
+  });
+}
+
+array1 = [1,2,3,4];
+
+console.log('Exercici 4:');
+processarElements(array1, imprimir);
+

66644fd64956aea85ece468b6ec9ffd99b715afe
Mesadova <emesadova@hotmail.com>
2024-10-07 10:05:08 +0200

    Nivell 2

--- 1.2-OperadorTernari.js
+++ 1.2-OperadorTernari.js
@@ -12,3 +12,26 @@ compareNumbers = (num1, num2) => num1 > num2 ? 'Number 1 is bigger.' : 'Number 2
 
 console.log('Exercici 2:');
 console.log(compareNumbers(5,99));
+
+
+// === Nivell 2 ===
+// === Exercici 3 ===
+
+calcNumberSign = (num) => {
+    return Math.sign(num) > 0 ? 'Positiu.'
+        : Math.sign(num) < 0 ? 'Negatiu.'
+        : Math.sign(num) == 0 ? 'Zero.'
+        : 'error';
+  }
+
+trobarMaxim = (a, b, c) => {
+    return a > b & a > c ? `A (${a}) és el nombre més gran.`
+        : b > a & b > c ? `B (${b}) és el nombre més gran.`
+        : c > a & c > b ? `C (${c}) és el nombre més gran.`
+        : 'Error: No hi ha un nombre més gran que els altres dos.';
+  }
+
+  console.log('Exercici 3:');
+  console.log(calcNumberSign(-3));
+  console.log(trobarMaxim(2,1,9));
+  

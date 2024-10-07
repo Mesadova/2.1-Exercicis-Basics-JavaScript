e23d38be329d0f9b20b5f3c14938942a340dd2fc
Mesadova <emesadova@hotmail.com>
2024-10-07 10:23:07 +0200

    Nivell 2

--- 1.4-Rest&Spread.js
+++ 1.4-Rest&Spread.js
@@ -23,3 +23,29 @@ suma = (...args) => {
 console.log('Exercici 2:');
 console.log(suma(1,2,3,4));
 
+
+// === Nivell 2 ===
+// === Exercici 3 ===
+
+const objecte1 = { foo: "bar", x: 42 };
+const objecte2 = {...objecte1};
+
+Object.assign(objecte2, { foo: 'ooo'}, { x: 1111});
+
+
+console.log('Exercici 3:');
+console.log(objecte1);
+console.log(objecte2);
+
+
+// === Exercici 4 ===
+
+array1 = ['a', 2, 'b', 4, 'c', 6];
+
+const[x, y, ...rest] = array1
+
+
+console.log('Exercici 4:');
+console.log(x);
+console.log(y);
+console.log(rest);

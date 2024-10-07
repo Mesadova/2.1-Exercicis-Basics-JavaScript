e342aa9c9d8c4571644d7d5c0fcd53e1903edb10
Mesadova <emesadova@hotmail.com>
2024-10-07 10:39:21 +0200

    Nivell 1

---
+++ 1.7-Promises&AsyncAwait.js
@@ -0,0 +1,51 @@
+// === Nivell 1 ===
+// === Exercici 1 ===
+
+let promise1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Hola, món');
+    }, 2000);
+});
+
+
+// === Exercici 2 ===
+
+promise1.then((resolvedValue) => {
+    console.log('Exercici 2:', resolvedValue);
+});
+
+
+// === Exercici 3 ===
+let input = 'Hola, món';
+
+let promise2 = new Promise(function(resolve, reject) {
+    if (input == 'Hola') {
+        setTimeout(() => {
+            resolve('Resolved');
+        }, 2000);
+    } else {
+        reject('Exercici 3: Rejected');
+    }
+});
+
+promise2.then(
+    (result) => { 
+       console.log(result);
+    },
+    (error) => { 
+       console.log(error);
+    }
+);
+
+
+// === Exercici 4 ===
+
+async function asyncCall() {
+    console.log('calling exercici 4...');
+    const result = await promise1;
+    console.log('executed exercici 4...')
+    console.log('Exercici 4 result:', result);;
+}
+
+asyncCall();
+

13ec3471fe600ec96c2d0528e6a12ac7138357e8
Mesadova <emesadova@hotmail.com>
2024-10-07 10:02:40 +0200

    Nivell 1

---
+++ 1.2-OperadorTernari.js
@@ -0,0 +1,14 @@
+// === Nivell 1 ===
+// === Exercici 1 ===
+
+potConduir = (edat) => edat >= 18 ? 'Pots conduir.' : 'No pots conduir';
+
+console.log('Exercici 1:')
+console.log(potConduir(18));
+
+// === Exercici 2 ===
+
+compareNumbers = (num1, num2) => num1 > num2 ? 'Number 1 is bigger.' : 'Number 2 is bigger.';
+
+console.log('Exercici 2:');
+console.log(compareNumbers(5,99));
